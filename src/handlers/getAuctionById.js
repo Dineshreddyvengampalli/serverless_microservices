67d49dfcb6bdb16c970f59101949b02f83adc05e
Dineshreddyvengampalli <dineshreddyvengampalli@gmail.com>
2023-03-07 22:23:58 +0530

    fixed getting undefined from query

--- src/handlers/getAuctionById.js
+++ src/handlers/getAuctionById.js
@@ -4,15 +4,15 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 
 async function getAuctionById(event, context) {
-    let id = event.pathParameters;
-    console.log('id', id)
+    let {id} = event.pathParameters;
     let auction;
     try {
         const result =  await dynamodb.get({
             TableName : process.env.AUCTION_TABLE_NAME,
             Key :{id}
-        })
+        }).promise()
         auction = result.Item
+        console.log('result.Item',result.Item)
     } catch (error) {
         throw new createError.InternalServerError(error)
     }
