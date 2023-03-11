33885070d9d7d65b4a7cd5a9cc629f15f23475b2
Dineshreddyvengampalli <dineshreddyvengampalli@gmail.com>
2023-03-11 19:53:37 +0530

    added global secondary index for auctions table

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,27 @@
+import AWS from 'aws-sdk';
+import createError from 'http-errors'
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
 async function processAuctions(event,context){
-    console.log('this is process Auctions function')
+    const now = new Date()
+    const params = {
+        TableName: process.env.AUCTION_TABLE_NAME,
+        IndexName: 'statusAndEndDate',
+        KeyConditionExpression: '#status = :status AND endingAt <= :now',
+        ExpressionAttributeValues: {
+            ':status': 'OPEN',
+            ':now': now.toISOString()
+        },
+        ExpressionAttributeNames: {
+            '#status': 'status'
+        }
+    }
+    try {
+        const response = await dynamoDb.query(params).promise()
+        const result = response.Items
+        console.log(result)
+    } catch (error) {
+        throw new createError.InternalServerError(error)
+    }
 }
 
 
